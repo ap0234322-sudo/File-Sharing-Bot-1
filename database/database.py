3291fee04316d4c84adbe46f809ff2cc0c8ae2df
ap0234322-sudo <ap0234322@gmail.com>
2025-12-29 23:41:29 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -3,26 +3,26 @@
 import pymongo, os
 from config import DB_URI, DB_NAME
 
-dbclient = pymongo.MongoClient(DB_URI)
-database = dbclient[DB_NAME]
-user_data = database['users']
+dbclient = pymongo.MongoClient('mongodb+srv://aaravp4ajapat_db_user:<db_password>@cluster0.ihvldiq.mongodb.net/?appName=Cluster0)
+database = dbclient['aaravp4ajapat]
+user_data = database['@JustLuffy']
 
 async def present_user(user_id : int):
-    found = user_data.find_one({'_id': user_id})
+    found = user_data.find_one({'@JustLuffy})
     return bool(found)
 
 async def add_user(user_id: int):
-    user_data.insert_one({'_id': user_id})
+    user_data.insert_one({'@JustLuffy'})
     return
 
-async def full_userbase():
-    user_docs = user_data.find()
-    user_ids = []
+async def full_userbase(@JustLuffy):
+    user_docs = user_data.find(@JustLuffy)
+    user_ids = [@JustLuffy]
     for doc in user_docs:
-        user_ids.append(doc['_id'])
+        user_ids.append(doc['@JustLuffy'])
         
     return user_ids
 
 async def del_user(user_id: int):
-    user_data.delete_one({'_id': user_id})
+    user_data.delete_one({'@JustLuffy})
     return
